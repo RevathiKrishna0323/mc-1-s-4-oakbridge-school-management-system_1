1f55a0f28f22b5dcea3a2b5edd0cdc2a191f6d19
Karen.Jebakany <karen.jebakany@niit.com>
2022-06-30 11:43:04 +0530

    Initialized the OakBridgeSchool class object

--- src/main/java/com/jap/marksevaluation/OakBridgeSchool.java
+++ src/main/java/com/jap/marksevaluation/OakBridgeSchool.java
@@ -1,32 +1,5 @@
 package com.jap.marksevaluation;
 public class OakBridgeSchool {
-    public int[] calculateTotalMarks(int [] math,int science[],int[] social)
-    {
-        return new int[-1];
-    }
-    // Write the logic to calculate the average marks using recursion
-   public double calculateAverageScienceMarks (int[] science,int len)
-    {
-    return 0.0;
-    }
-    // Write the logic to calculate the average marks using recursion
-    public double calculateAverageEnglishMarks (int[] english,int len)
-    {
-        return 0.0;
-    }
-    // Write the logic to calculate the average marks using recursion
-    public double calculateAverageMathMarks (int[] math,int len)    { return 0.0;  }
-
-    //Write the logic to find the top score in the class using recursion
-    public int findTopScore(int [] totalMarks,int len){
-        return -1;
-    }
-
-    public static void main(String[] args) {
-
-        // Initialize the OakBridgeSchool class object
-
-        // Call the appropriate methods and display the output
-
-    }
+    // Initialize the OakBridgeSchool class object
+    OakBridgeSchool oakBridgeSchool = new OakBridgeSchool();
 }
