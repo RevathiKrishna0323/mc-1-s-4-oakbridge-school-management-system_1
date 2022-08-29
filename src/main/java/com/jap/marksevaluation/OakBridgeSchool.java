b96e79f19f659beb150722b0f2ea404a6a079906
Revathi G <revathikrishna0323@gmail.com>
2022-08-29 18:20:37 +0530

    declared int arrays

--- src/main/java/com/jap/marksevaluation/OakBridgeSchool.java
+++ src/main/java/com/jap/marksevaluation/OakBridgeSchool.java
@@ -2,4 +2,9 @@ package com.jap.marksevaluation;
 public class OakBridgeSchool {
     // Initialize the OakBridgeSchool class object
     OakBridgeSchool oakBridgeSchool = new OakBridgeSchool();
+    int[] math = {88, 89, 100, 70, 60, 80, 35, 3, 25, 56};
+    int[] science = {80, 83, 99, 67, 56, 84, 38, 9, 32, 65};
+    int[] english = {90, 98, 100, 65, 54, 82, 40, 13, 45, 67};
+    int len = math.length;
+
 }
